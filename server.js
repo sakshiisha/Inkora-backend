a40a8c5de8eafb057ff479605fb4b27bd9f0cd1c
Sakshi Sharma <sakshisha11245@gmail.com>
2026-07-22 00:15:35 +0530

    new

--- server.js
+++ server.js
@@ -8,7 +8,24 @@ const blogRoutes = require('./routes/blogRoutes')
 
 const app = express()
 
-app.use(cors())
+const allowedOrigins = [
+  'http://localhost:5173',
+  'inkora-frontend.vercel.app',
+]
+
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true)
+      } else {
+        callback(new Error('Not allowed by CORS'))
+      }
+    },
+    credentials: true,
+  })
+)
+
 app.use(express.json())
 
 app.use('/api/auth', authRoutes)
@@ -21,7 +38,7 @@ app.get('/', (req, res) => {
 const PORT = process.env.PORT || 5000
 
 sequelize
-  .sync({ alter: true }) // auto-creates/updates SQL tables from models
+  .sync({ alter: true })
   .then(() => {
     console.log('MySQL connected & tables synced')
     app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
